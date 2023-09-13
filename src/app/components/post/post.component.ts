0df50c16c7b00e7d2495f7d7da7a96930358b341
AndreSDS <andre_smiths@outlook.com>
2023-09-12 23:34:50 -0300

    refatoring post component

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-post',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent {
+  postMaxLength: number = 240;
+  postRemains: number = this.postMaxLength;
+  postInput !: FormControl;
 
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+    this.postInput = this.formBuilder.control({
+      value: '',
+      disabled: false
+    });
+  }
+
+  setPostRemains() {
+    this.postRemains = this.postMaxLength - this.postInput.value.length;
+  }
+
+  sendPost() {
+    alert(this.postInput.value);
+  }
 }
